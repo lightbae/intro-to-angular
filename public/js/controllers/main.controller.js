dccebe2d5a750334b6f148d12986f83d249af7d1
Harryson Pointdujour <Harryson Pointdujour>
2016-10-25 18:48:51 -0400

    added the ability edit and save todo li's

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -9,6 +9,8 @@ function MainController ($scope, ToDoService){ //$scope is our bridge to the DOM
   $scope.todos = ToDoService.get(); //gimme this data and put it in the todos variable
   $scope.createTodo = createTodo;
   $scope.deleteTodo = deleteTodo;
+  $scope.editTodo = editTodo;
+  $scope.saveTodo = saveTodo;
 
   function createTodo(newTodo){
     ToDoService.create(newTodo);
@@ -16,7 +18,14 @@ function MainController ($scope, ToDoService){ //$scope is our bridge to the DOM
   }
   function deleteTodo(index){
     ToDoService.delete(index);
-    
+  }
+  function editTodo(todo){
+    todo.isBeingEdited = true;
+  }
+
+  function saveTodo(index, todo){
+    ToDoService.update(index, todo.desc)
+    todo.isBeingEdited = false;
   }
 }
 }());
